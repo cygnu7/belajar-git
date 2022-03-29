3d6307c00d5e52cbd637f01f3bcbab5d7f9893ac
cygnu7 <cygnus7seven@gmail.com>
2022-03-29 11:54:59 +0700

    My Commit

---
+++ basic/01_git_add_and_git_commit.sh
@@ -0,0 +1,18 @@
+# Show Git Status
+cygnus7$ git log
+commit 90da9b4efbfe9cc759bdcc94fe3d91b76f4781e2 (HEAD)
+Author: Cygnus7 <cygnus7@This-MacBook-Air.local>
+Date:   Wed Mar 23 15:56:09 2022 +0700
+
+    my first commit
+(base) This-MacBook-Air:belajar-git cygnus7$ git status
+HEAD detached at 90da9b4
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+	.DS_Store
+	basic/
+
+nothing added to commit but untracked files present (use "git add" to track)
+
+# Add
+cygnus7$ git add .
