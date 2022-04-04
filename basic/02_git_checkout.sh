67bee9ddcc97f184177f036a99beeb7018f54224
cygnu7 <cygnus7seven@gmail.com>
2022-04-04 16:44:24 +0700

    Learn checkout with code

--- basic/02_git_checkout.sh
+++
@@ -1,9 +0,0 @@
-# Show different from a file
-$ git diff
-diff --git a/basic/02_git_checkout.sh b/basic/02_git_checkout.sh
-index e69de29..8318c86 100644
---- a/basic/02_git_checkout.sh
-+++ b/basic/02_git_checkout.sh
-
-# Back to original file
-$ git checkout 02_git_checkout.sh
