bdc21ad9331ef165e098a719000cf07cac52065c
cygnu7 <cygnus7seven@gmail.com>
2022-07-13 15:34:06 +0700

    branch baru

---
+++ basic/06_github.sh
@@ -0,0 +1,22 @@
+# Repository
+Create Repository at github website.
+
+# Clone
+## new-name is optional
+$ git clone https://github.com/cygnu7/belajar-git.git [new-name]
+
+# Push
+$ git push origin master
+
+# Pull
+$ git pull
+
+# Branch
+$ git checkout -b branchbaru
+branch baru
+
+# Merge
+
+# Fork
+
+# .gitignore
